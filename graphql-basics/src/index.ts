bd14841ccddafa60ce0711383f5eee2c907e707f
david <andrianarivo.david@gmail.com>
2023-10-31 18:45:51 +0300

    Creating Your Own GraphQL API

--- graphql-basics/src/index.ts
+++ graphql-basics/src/index.ts
@@ -1,8 +1,44 @@
-import myCurrentLocation, { getGreeting, message, name } from './myModule'
-import myAddFunction, { subtract } from './math'
+import { createYoga } from 'graphql-yoga'
+import { createSchema } from 'graphql-yoga'
+import { createServer } from 'http'
 
-console.log(message, name, myCurrentLocation)
-console.log(getGreeting('David'))
+// Type definitions (schema)
+const typeDefs = `
+  type Query {
+    hello: String!
+    name: String!
+    location: String!
+    bio: String!
+  }
+`
 
-console.log("Add:", myAddFunction(1, 2))
-console.log("Subtract:", subtract(1, 2))
+// Resolvers
+const resolvers = {
+  Query: {
+    hello() {
+      return 'This is my first query!'
+    },
+    name() {
+      return 'David Andrianarvio'
+    },
+    location() {
+      return 'Madagascar'
+    },
+    bio() {
+      return 'I am a 28 years old professional from Madagascar'
+    }
+  }
+}
+
+const schema = createSchema({
+  typeDefs,
+  resolvers
+})
+
+const yoga = createYoga({ schema })
+
+const server = createServer(yoga)
+
+server.listen(4000, () => {
+  console.info('Server is running on http://localhost:4000/graphql')
+})
