3f583ce40b002a77c2a10cd68b687d505c6dec42
David Andrianarivo <andrianarivo.david@gmail.com>
2023-11-02 11:07:23 +0300

    Operation Arguments

--- graphql-basics/src/index.ts
+++ graphql-basics/src/index.ts
@@ -1,60 +1,63 @@
 import { createYoga } from 'graphql-yoga'
 import { createSchema } from 'graphql-yoga'
 import { createServer } from 'http'
+import {loadFiles} from "@graphql-tools/load-files";
+import {QueryGreetingArgs} from "./generated/graphql"
+import {GraphQLResolveInfo} from "graphql"
+import { Resolvers } from './generated/graphql'
 
-// Type definitions (schema)
-const typeDefs = `
-  type Query {
-    me: User!
-    post: Post!
-  }
-  
-  type User {
-    id: ID!
-    name: String!
-    email: String!
-    age: Int
-  }
-  
-  type Post {
-    id: ID!
-    title: String!
-    body: String!
-    published: Boolean!
-  }
-`
+async function main() {
 
-// Resolvers
-const resolvers = {
-  Query: {
-    me() {
-      return {
-        id: '123098',
-        name: 'Mike',
-        email: 'mike@example.com',
-        age: 28
-      }
-    },
-    post() {
-      return {
-        id: '092',
-        title: 'GraphQL 101',
-        body: '',
-        published: false
+  // Type definitions (schema)
+  const typeDefs = await loadFiles('**/*.graphql')
+
+  // Resolvers
+  const resolvers: Resolvers = {
+    Query: {
+      greeting(parent, args, ctx, info) {
+        if(args.name) {
+          return `Hello ${args.name}!`
+        } else {
+          return 'Hello!' // default
+        }
+      },
+      add(_, args) {
+        return args.a + args.b
+      },
+      me() {
+        return {
+          id: '123098',
+          name: 'Mike',
+          email: 'mike@example.com',
+          age: 28
+        }
+      },
+      post() {
+        return {
+          id: '092',
+          title: 'GraphQL 101',
+          body: '',
+          published: false
+        }
       }
     }
   }
+
+  const schema = createSchema({
+    typeDefs,
+    resolvers
+  })
+
+  const yoga = createYoga({ schema })
+
+  const server = createServer(yoga)
+
+  server.listen(4000, () => {
+    console.info('Server is running on http://localhost:4000/graphql')
+  })
 }
 
-const schema = createSchema({
-  typeDefs,
-  resolvers
-})
-
-const yoga = createYoga({ schema })
-
-const server = createServer(yoga)
-
-server.listen(4000, () => {
-  console.info('Server is running on http://localhost:4000/graphql')
+main().catch((err) => {
+  console.error(err)
+  process.exit(1)
 })
