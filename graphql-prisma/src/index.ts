e18a5e0fb81d45935d41cbca876c4aff9af8b4d0
david <andrianarivo.david@gmail.com>
2023-11-07 13:48:57 +0300

    Client Create Operations

--- graphql-prisma/src/index.ts
+++ graphql-prisma/src/index.ts
@@ -1,8 +1,44 @@
 import { PrismaClient } from '@prisma/client'
-const prisma = new PrismaClient()
+const prisma = new PrismaClient({ log: ['query']})
 
 async function main() {
-  const user = await prisma.user.findMany()
+  await prisma.user.deleteMany()
+  const users = await prisma.user.createMany({
+    data: [{
+      name: 'Bob',
+      email: 'bob@example.com',
+      age: 25
+    }, {
+      name: 'Queen',
+      email: 'spades@card.com',
+      age: 55
+    }]
+  })
+  console.log(users)
+  const user = await prisma.user.create({
+    data: {
+      name: 'Alice',
+      email: 'alice@wonderland.com',
+      age: 14,
+      userPreference: {
+        create: {
+          emailUpdates: true,
+        }
+      }
+    },
+    /*include: {
+      userPreference: true,
+    }*/
+    select: {
+      id: true,
+      name: true,
+      userPreference: {
+        select: {
+          emailUpdates: true,
+        }
+      }
+    }
+  })
   console.log(user)
 }
 
