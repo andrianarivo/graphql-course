78c2065e748b6aca74d21da2f700c441ca90739a
david <andrianarivo.david@gmail.com>
2023-11-07 14:27:27 +0300

    Connect existing Relationship

--- graphql-prisma/src/index.ts
+++ graphql-prisma/src/index.ts
@@ -2,18 +2,30 @@ import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient({ log: ['query']})
 
 async function main() {
+  const userPrefs = await prisma.userPreference.findMany()
   const user = await prisma.user.update({
     where: {
       id: 7
     },
     data: {
-      email: 'test@example.com',
-      age: {
-        increment: 1
+      userPreference: {
+        connect: {
+          id: userPrefs[0].id
+        }
       }
     }
   })
-  console.log(user)
+  const updatedUser = await prisma.user.update({
+    where: {
+      id: 7
+    },
+    data: {
+      userPreference: {
+        disconnect: true
+      }
+    }
+  })
+  console.log(user, updatedUser)
 }
 
 main().catch(e => {
