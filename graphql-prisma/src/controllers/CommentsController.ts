165e0e7006beaba182e94368c6839de39b21f71a
David Andrianarivo <andrianarivo.david@gmail.com>
2023-11-09 11:05:17 +0300

    Migrate Comments and Posts to pothos

--- graphql-prisma/src/controllers/CommentsController.ts
+++ graphql-prisma/src/controllers/CommentsController.ts
@@ -1,11 +1,121 @@
 import { builder} from "../builder";
 import {prisma} from "../db";
+import {GraphQLError} from "graphql/error";
 
 builder.queryField('comments', t =>
   t.prismaField({
     type: ['Comment'],
     resolve: (query, parent, args, context, info) => {
       return prisma.comment.findMany({...query})
+    }
+  })
+)
+
+const CreateCommentInput = builder.inputType('CreateCommentInput', {
+  fields: (t) => ({
+    text: t.string({required: true}),
+    author: t.int({required: true}),
+    post: t.int({required: true})
+  })
+})
+
+builder.mutationField('createComment', t =>
+  t.prismaField({
+    type: 'Comment',
+    args: {
+      data: t.arg({
+        type: CreateCommentInput,
+        required: true
+      })
+    },
+    resolve: async (query, parent, args, context, info) => {
+      const userExists = await prisma.user.findUnique({
+        where: {
+          id: args.data.author
+        }
+      })
+      if(!userExists) {
+        throw new GraphQLError('User not found.')
+      }
+      const postExists = await prisma.post.findUnique({
+        where: {
+          id: args.data.post
+        }
+      })
+      if(!postExists) {
+        throw new GraphQLError('Post not found.')
+      }
+      return prisma.comment.create({
+        ...query,
+        data: {
+          text: args.data.text,
+          authorId: args.data.author,
+          postId: args.data.post
+        }
+      })
+    }
+  })
+)
+
+const UpdateCommentInput = builder.inputType('UpdateCommentInput', {
+  fields: (t) => ({
+    text: t.string({required: false})
+  })
+})
+
+builder.mutationField('updateComment', t =>
+  t.prismaField({
+    type: 'Comment',
+    args: {
+      id: t.arg.int({required: true}),
+      data: t.arg({
+        type: UpdateCommentInput,
+        required: true
+      })
+    },
+    resolve: async (query, parent, args, context, info) => {
+      const originalComment = await prisma.comment.findUnique({
+        where: {
+          id: args.id
+        }
+      })
+      if(!originalComment) {
+        throw new GraphQLError('Comment not found.')
+      }
+      return prisma.comment.update({
+        ...query,
+        where: {
+          id: args.id
+        },
+        data: {
+          text: args.data.text || originalComment.text
+        }
+      })
+    }
+  })
+)
+
+builder.mutationField('deleteComment', t =>
+  t.prismaField({
+    type: 'Comment',
+    args: {
+      id: t.arg.int({required: true})
+    },
+    resolve: async (query, parent, args, context, info) => {
+      const comment = await prisma.comment.findUnique({
+        where: {
+          id: args.id
+        }
+      })
+      if(!comment) {
+        throw new GraphQLError('Comment not found.')
+      }
+      return prisma.comment.delete({
+        ...query,
+        where: {
+          id: args.id
+        }
+      })
     }
   })
 )
--- graphql-prisma/src/controllers/PostsController.ts
+++ graphql-prisma/src/controllers/PostsController.ts
@@ -1,11 +1,119 @@
 import { builder } from '../builder'
 import { prisma } from '../db'
+import {GraphQLError} from "graphql/error";
 
 builder.queryField('posts', t =>
   t.prismaField({
     type: ['Post'],
     resolve: (query, parent, args, context, info) => {
       return prisma.post.findMany({...query})
+    }
+  })
+)
+
+const CreatePostInput = builder.inputType('CreatePostInput', {
+  fields: (t) => ({
+    title: t.string({required: true}),
+    body: t.string({required: false}),
+    published: t.boolean({required: false}),
+    author: t.int({required: true})
+  })
+})
+
+builder.mutationField('createPost', t =>
+  t.prismaField({
+    type: 'Post',
+    args: {
+      data: t.arg({
+        type: CreatePostInput,
+        required: true
+      })
+    },
+    resolve: async (query, parent, args, context, info) => {
+      const userExists = await prisma.user.findUnique({
+        where: {
+          id: args.data.author
+        }
+      })
+      if(!userExists) {
+        throw new GraphQLError('User not found.')
+      }
+      return prisma.post.create({
+        ...query,
+        data: {
+          title: args.data.title,
+          body: args.data.body || '',
+          published: args.data.published || false,
+          authorId: args.data.author
+        }
+      })
+    }
+  })
+)
+
+const UpdatePostInput = builder.inputType('UpdatePostInput', {
+  fields: (t) => ({
+    title: t.string({required: false}),
+    body: t.string({required: false}),
+    published: t.boolean({required: false})
+  })
+})
+
+builder.mutationField('updatePost', t =>
+  t.prismaField({
+    type: 'Post',
+    args: {
+      id: t.arg.int({required: true}),
+      data: t.arg({
+        type: UpdatePostInput,
+        required: true
+      })
+    },
+    resolve: async (query, parent, args, context, info) => {
+      const originalPost = await prisma.post.findUnique({
+        where: {
+          id: args.id
+        }
+      })
+      if(!originalPost) {
+        throw new GraphQLError('Post not found.')
+      }
+      return prisma.post.update({
+        ...query,
+        where: {
+          id: args.id
+        },
+        data: {
+          title: args.data.title || originalPost.title,
+          body: args.data.body || originalPost.body,
+          published: args.data.published || originalPost.published
+        }
+      })
+    }
+  })
+)
+
+builder.mutationField('deletePost', t =>
+  t.prismaField({
+    type: 'Post',
+    args: {
+      id: t.arg.int({required: true})
+    },
+    resolve: async (query, parent, args, context, info) => {
+      const originalPost = await prisma.post.findUnique({
+        where: {
+          id: args.id
+        }
+      })
+      if(!originalPost) {
+        throw new GraphQLError('Post not found.')
+      }
+      return prisma.post.delete({
+        ...query,
+        where: {
+          id: args.id
+        }
+      })
     }
   })
 )
