7db1efca0312eacd032e37135fa78c3f1ba975cf
David Andrianarivo <andrianarivo.david@gmail.com>
2023-11-27 21:46:44 +0300

    Locking Down Individual Type Fields

--- graphql-prisma/src/models/User.ts
+++ graphql-prisma/src/models/User.ts
@@ -1,10 +1,19 @@
 import { builder } from '../builder'
+import {getUserId} from "../controllers/concerns/GetUserId";
 
 builder.prismaObject('User', {
   fields: t => ({
       id: t.exposeID('id'),
       name: t.exposeString('name', { nullable: true }),
-      email: t.exposeString('email'),
+      email: t.string({
+        resolve: (user, args, { jwt }, info) => {
+          const userId = getUserId(jwt, false)
+          if(userId === user.id) {
+            return user.email
+          }
+          return ''
+        }
+      }),
       age: t.exposeInt('age'),
       posts: t.relation('posts'),
       comments: t.relation('comments')
